06cae0a80d6fe25df2b27aed885976f3f00fbf3a
Jufebrown <jufebrown@gmail.com>
2017-10-31 16:38:20 -0500

    ran through tut with class

---
+++ auth/local.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const moment = require("moment");
+const jwt = require("jwt-simple");
+
+const encodeToken = user => {
+  const payload = {
+    exp: moment()
+      .add(14, "days")
+      .unix(),
+    iat: moment.unix(),
+    sub: user.id
+  };
+  return jwt.encode(payload, process.env.TOKEN_SECRET);
+};
+
+//decodes token
+function decodeToken(token, callback) {
+  const payload = jwt.decode(token, process.env.TOKEN_SECRET);
+  const now = moment().unix();
+  // check if the token has expired
+  if (now > payload.exp) callback("Token has expired.");
+  else callback(null, payload);
+}
+
+module.exports = {
+  encodeToken,
+  decodeToken
+};
+
